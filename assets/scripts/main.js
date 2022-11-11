2f08e62b07c5a136be870e5facb57e06d41d630f
SideCoin <yreganyang@gmail.com>
2022-11-10 18:09:48 -0800

    Worked on part 2

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -45,9 +45,19 @@ function initializeServiceWorker() {
   // We first must register our ServiceWorker here before any of the code in
   // sw.js is executed.
   // B1. TODO - Check if 'serviceWorker' is supported in the current browser
+  if('serviceWorker' in navigator) {
+    window.addEventListener('load', ()=> {
+      navigator.serviceWorker.register('./sw.js').then((register)=> {
+        console.log('Successfully registered');
+      },
+      (err)=> {
+        console.error(`Registration failed: ${err}`);
+      });
+    });
+  }
   // B2. TODO - Listen for the 'load' event on the window object.
   // Steps B3-B6 will be *inside* the event listener's function created in B2
-  // B3. TODO - Register '/sw.js' as a service worker (The MDN article
+  // B3. TODO - Register './sw.js' as a service worker (The MDN article
   //            "Using Service Workers" will help you here)
   // B4. TODO - Once the service worker has been successfully registered, console
   //            log that it was successful.
@@ -108,7 +118,7 @@ async function getRecipes() {
         //            we have provided. Then, pass the recipes array to the Promise's
         //            resolve() method.
         if (recipes.length === RECIPE_URLS.length) {
-          localStorage.setItem("recipes", recipes);
+          //localStorage.setItem("recipes", recipes); //not sure if this is neccesary 
           saveRecipesToStorage(recipes);
           resolve(recipes);
         }
